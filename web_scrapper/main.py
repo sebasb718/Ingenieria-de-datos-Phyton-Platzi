34cb5433fb7b9c22d21b9cdae2fed4ade2e31b29
Juan Sebastian Bernal Cubides <sebasb718@gmail.com>
2020-07-13 23:13:24 -0500

    Hasta clase 19, trayendo titulos y contenido de las noticias

--- web_scrapper/main.py
+++ web_scrapper/main.py
@@ -1,20 +1,58 @@
 import argparse
 import logging
 import news_page_objects as news
+import re
 
 from common import config
 
+from requests.exceptions import HTTPError
+from urllib3.exceptions import MaxRetryError
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+is_well_formed_link = re.compile(r'^https?://.+/.+') # https://url.com/link
+is_root_path = re.compile(r'^/.+$') # /link
 
 def _news_scraper(news_site_uid):
   host = config()['news_sites'][news_site_uid]['url']
   logging.info('Beggining scraper for {}'.format(host))
   homepage = news.HomePage(news_site_uid, host)
 
+  articles = []
   for link in homepage.article_link:
-    print(link)
+    article = _fetch_article(news_site_uid, host, link)
+    
+    if article:
+      logger.info('Article fetched')
+      articles.append(article)
+      print(article.title)
+      
+  print(len(articles))
 
+def _fetch_article(news_site_uid, host, link):
+  logger.info('Start fetching article at {}'.format(link))
+  article = None
+  try:
+    logger.info('Real link is {}'.format(_build_link(host, link)))
+    article = news.ArticlePage(news_site_uid, _build_link(host, link))
+  except (HTTPError, MaxRetryError) as e:
+    logger.warning('Error while fetching the article', exc_info=False)
+
+  if article and not article.body:
+    logger.warning('Invalid article. There is no body')
+    return None
+  
+  return article
+
+def _build_link(host, link):
+  if is_well_formed_link.match(link):
+    return link
+  elif is_root_path.match(link):
+    return '{}{}'.format(host, link)
+  else:
+    return '{}/{}'.format(host, link)
+  
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   news_site_choices = list(config()['news_sites'].keys())
