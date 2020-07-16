8d4d1ff33149797db4080e0f2864bc03a4c92dc5
Juan Sebastian Bernal Cubides <sebasb718@gmail.com>
2020-07-15 22:34:33 -0500

    Hasta clase 34, finalizado script para limpiar archivos y algo de analisis de los mismos

--- web_scrapper/newspaper_recipe.py
+++ web_scrapper/newspaper_recipe.py
@@ -3,6 +3,8 @@ import logging
 from urllib.parse import urlparse
 import pandas as pd
 import hashlib
+import nltk
+from nltk.corpus import stopwords
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -16,6 +18,11 @@ def main(filename):
   df = _fill_missing_titles(df)
   df = _generate_uids_for_rows(df)
   df = _remove_non_desired_characters(df)
+  df = _add_tokenized_colum(df, 'title')
+  df = _add_tokenized_colum(df, 'body')
+  df = _remove_duplicates_for_column(df, 'title')
+  df = _drop_rows_with_missing_data(df)
+  _save_data(df, filename)
   return df
 
 def _read_data(filename):
@@ -30,7 +37,7 @@ def _extract_newspaper_uid(filename):
 
 def _add_newspaper_uid_column(df, newspaper_uid):
   logger.info('Filling newspaper_uid column with: {}'.format(newspaper_uid))
-  df['newspapaer_uid'] = newspaper_uid
+  df['newspaper_uid'] = newspaper_uid
   return df
 
 def _extract_and_add_host(df):
@@ -65,6 +72,34 @@ def _remove_non_desired_characters(df):
   df['body'] = stripped_body
   return df
 
+def _remove_duplicates_for_column(df, column_name):
+  logger.info('Removing duplicates for column {}'.format(column_name))
+  return df.drop_duplicates(subset=[column_name], keep='first')
+
+def _add_tokenized_colum(df, column_name):
+  logger.info('Tokenizing column {} and adding it to the dataframe'.format(column_name))
+  stop_words = set(stopwords.words('spanish'))
+  tokenized_column = (df
+                      .dropna()
+                      .apply(lambda row: nltk.word_tokenize(row[column_name]), axis=1)
+                      .apply(lambda tokens: list(filter(lambda token: token.isalpha(), tokens)))
+                      .apply(lambda tokens: list(map(lambda token: token.lower(), tokens)))
+                      .apply(lambda word_list: list(filter(lambda word: word not in stop_words, word_list)))
+                      .apply(lambda valid_world_list: len(valid_world_list))
+                     )
+  tokenized_column_name = 'n_tokens_{}'.format(column_name)
+  df[tokenized_column_name] = tokenized_column
+  return df
+
+def _drop_rows_with_missing_data(df):
+  logger.info('Removing rows with missing values')
+  return df.dropna()
+
+def _save_data(df, file_name):
+  final_name = 'clean_{}'.format(file_name)
+  logger.info('Saving cleaned data in file {}'.format(final_name))
+  df.to_csv(final_name)
+
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   parser.add_argument('filename',
@@ -73,5 +108,3 @@ if __name__ == "__main__":
   args = parser.parse_args()
   df = main(args.filename)
   print(df)
-  #df.to_csv(r'modified_{}'.format(args.filename))
-  #print('File saved')
